a8df75bbaad86609a785d847abcb5f635a789704
vansh Sharma <vanshsharma7832@gmail.com>
2026-02-27 22:17:03 +0530

    fixed int str error

--- main.py
+++ main.py
@@ -65,4 +65,4 @@ async def translate_sentence(request: Request, body: TranslationRequest):
         return {"data": result}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=os.getenv("HOST", "0.0.0.0"), port=os.getenv("PORT", 8000), reload=True)
+    uvicorn.run("main:app", host=os.getenv("HOST", "0.0.0.0"), port=int(os.getenv("PORT", 8000)), reload=True)
